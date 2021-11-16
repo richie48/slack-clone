8cbc8b01ad3904b874db8b5c197b651e028be272
richie48 <odiorrichard@gmail.com>
2021-11-16 20:03:40 +0000

    housekeeping

---
+++ slack_clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC-K8eutHB1d8qEwh0MSg7uYN5UfIbYx8g",
+    authDomain: "slack-clone-8c4ca.firebaseapp.com",
+    projectId: "slack-clone-8c4ca",
+    storageBucket: "slack-clone-8c4ca.appspot.com",
+    messagingSenderId: "954707558460",
+    appId: "1:954707558460:web:7818992a549bff863c4339"
+  };
