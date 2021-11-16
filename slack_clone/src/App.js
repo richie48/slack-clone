5f0e1b2a279ead6007f4a985a53464a6d4869811
richie48 <odiorrichard@gmail.com>
2021-11-16 20:35:59 +0000

    added react router

--- slack_clone/src/App.js
+++ slack_clone/src/App.js
@@ -1,9 +1,17 @@
-
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App"><h1>Lets build slack</h1>
+    <div>
+    <Router>
+      <>
+        <Routes>
+          <Route path="/"><React.Fragment>This is the homepage</React.Fragment></Route>
+        </Routes>
+      </>
+    </Router>
     </div>
   );
 }
