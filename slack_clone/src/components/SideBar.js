b1221648dfb9c6a9f792b2de0784d9cb38f40882
richie48 <odiorrichard@gmail.com>
2021-11-22 16:35:36 +0000

    last commit

--- slack_clone/src/components/SideBar.js
+++ slack_clone/src/components/SideBar.js
@@ -9,20 +9,26 @@ import PeopleAltIcon from '@material-ui/icons/PeopleAlt'
 import AppsIcon from '@material-ui/icons/Apps'
 import FileCopyIcon from '@material-ui/icons/FileCopy'
 import ExpandLessIcon from '@material-ui/icons/ExpandLess'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import AddIcon from '@material-ui/icons/Add'
 
 
 function SideBar() {
     return (
         <div className='sidebar'>
             <SideBarHeader />
-            <SideBarOption Icon={InsertCommentIcon} title='Threads' />
-            <SideBarOption Icon={InboxIcon} title="Mentions and comments" />
-            <SideBarOption Icon={DraftsIcon} title='Saved items' />
-            <SideBarOption Icon={BookmarkBorderIcon} title='Channels' />
-            <SideBarOption Icon={PeopleAltIcon} title='People and user groups' />
-            <SideBarOption Icon={AppsIcon} title='Apps' />
-            <SideBarOption Icon={FileCopyIcon} title='File browser' />
+            <SideBarOption Icon={InsertCommentIcon} title='Threads' /><br />
+            <SideBarOption Icon={InboxIcon} title="Mentions and comments" /><br />
+            <SideBarOption Icon={DraftsIcon} title='Saved items' /><br />
+            <SideBarOption Icon={BookmarkBorderIcon} title='Channels' /><br />
+            <SideBarOption Icon={PeopleAltIcon} title='People and user groups' /><br />
+            <SideBarOption Icon={AppsIcon} title='Apps' /><br />
+            <SideBarOption Icon={FileCopyIcon} title='File browser' /><br />
             <SideBarOption Icon={ExpandLessIcon} title='Slow less' />
+            <hr />
+            <SideBarOption Icon={ExpandMoreIcon} title='Slow less' />
+            <hr />
+            <SideBarOption Icon={AddIcon} AddChannelOption title='Add channel' />
 
         </div>
     )
--- slack_clone/src/components/SideBarOption.js
+++ slack_clone/src/components/SideBarOption.js
@@ -1,9 +1,20 @@
+import { makeStyles } from "@material-ui/core"
 
+const useStyles = makeStyles({
+    main:
+    {
+        fontSize: 20,
+        marginRight: 5
+    }
+})
 
-function SideBarOption({ title, Icon }) {
-    return (
-        <div className='side-bar-options'><h3>{title}</h3> <Icon />
+function SideBarOption({ title, Icon, AddChannelOption }) {
+
+    const classes = useStyles();
+    return (<>
+        <div className='side-bar-options'><Icon className={classes.main} />{title}
         </div>
+    </>
     )
 }
 
